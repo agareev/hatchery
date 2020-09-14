12426b736542edaef92df7659f14d8416e28aa23
a.gareev <a.gareev@visionlabs.ru>
2020-09-14 16:24:34 +0300

    add uri's, add db support
    Signed-off-by: a.gareev <a.gareev@visionlabs.ru>

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module hatchery
 
 go 1.14
 
-require github.com/gorilla/mux v1.8.0 // indirect
+require (
+	github.com/gorilla/mux v1.8.0
+	go.mongodb.org/mongo-driver v1.4.1
+)
--- main.go
+++ main.go
@@ -1,27 +1,66 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
+
+var (
+	collection                            *mongo.Collection
+	dburl, dbport, dbname, collectionName string
+)
+
+func getEnv(key, defaultValue string) string {
+	value := os.Getenv(key)
+	if len(value) == 0 {
+		return defaultValue
+	}
+	return value
+}
 
 func names(w http.ResponseWriter, r *http.Request) {
 	log.Println("names")
 	fmt.Fprint(w, "names")
 }
 
+func init() {
+	dburl = getEnv("DBURL", "localhost")
+	dbport = getEnv("DBPORT", "27017")
+	dbname = getEnv("DBNAME", "testing")
+	collectionName = getEnv("COLLECTIONNAME", "numbers")
+}
+
 func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/", names)
+	router.HandleFunc("/name", names)
+	router.HandleFunc("/name/all", names)
+	router.HandleFunc("/name/x", names)
+	router.HandleFunc("/name/busy", names)
+	router.HandleFunc("/name/loose", names)
 	http.Handle("/", router)
 
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://"+dburl+":"+dbport))
+
+	collection = client.Database(dbname).Collection(collectionName)
+	ctx, _ = context.WithTimeout(context.Background(), 5*time.Second)
+	res, err := collection.InsertOne(ctx, bson.M{"name": "pi", "value": 3.14159})
+	id := res.InsertedID
+	log.Println(id)
+
 	fmt.Println("Server is listening... on :8182")
-	err := http.ListenAndServe(":8182", nil)
+	err = http.ListenAndServe(":8182", nil)
 	if err != nil {
 		log.Fatalln(err)
 	}
-
 }
